ca07f7f57d4fd273732b7af49340dc9d3179bf30
Feerko <Feerko@MacBook-Pro-Feerko.local>
2017-11-08 10:58:55 +0100

    parties only

--- src/us/_donut_/skuniversal/SkUniversal.java
+++ src/us/_donut_/skuniversal/SkUniversal.java
@@ -4,23 +4,7 @@ import ch.njol.skript.Skript;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
-import us._donut_.skuniversal.advancedban.AdvancedBanRegister;
-import us._donut_.skuniversal.advancedsurvivalgames.AdvancedSurvivalGamesRegister;
-import us._donut_.skuniversal.autorank.AutorankRegister;
-import us._donut_.skuniversal.bedwars.BedwarsRegister;
-import us._donut_.skuniversal.clearlagg.ClearLagRegister;
-import us._donut_.skuniversal.combatlog.CombatLogRegister;
-import us._donut_.skuniversal.lockette.LocketteRegister;
-import us._donut_.skuniversal.lockettepro.LocketteProRegister;
-import us._donut_.skuniversal.lwc.LWCRegister;
-import us._donut_.skuniversal.minepacks.MinePacksRegister;
 import us._donut_.skuniversal.parties.PartiesRegister;
-import us._donut_.skuniversal.plotsquared.PlotSquaredRegister;
-import us._donut_.skuniversal.prisonmines.PrisonMinesRegister;
-import us._donut_.skuniversal.pvplevels.PvPLevelsRegister;
-import us._donut_.skuniversal.shopkeepers.ShopkeepersRegister;
-import us._donut_.skuniversal.skywars_cookloco.SkywarsCookLocoRegister;
-import us._donut_.skuniversal.skywars_daboross.SkyWarsDaborossRegister;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -29,24 +13,7 @@ public class SkUniversal extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        Skript.registerAddon(this);
-        AdvancedBanRegister.registerAdvancedBan();
-        AdvancedSurvivalGamesRegister.registerAdvancedSurvivalGames();
-        AutorankRegister.registerAutorank();
-        BedwarsRegister.registerBedwars();
-        ClearLagRegister.registerClearLagg();
-        CombatLogRegister.registerCombatLog();
-        LocketteRegister.registerLockette();
-        LocketteProRegister.registerLockettePro();
-        LWCRegister.registerLWC();
-        MinePacksRegister.registerMinePacks();
         PartiesRegister.registerParties();
-        PlotSquaredRegister.registerPlotSquared();
-        PrisonMinesRegister.registerPrisonMines();
-        PvPLevelsRegister.registerPvPLevels();
-        ShopkeepersRegister.registerShopekeepers();
-        SkywarsCookLocoRegister.registerSkyWarsCookLoco();
-        SkyWarsDaborossRegister.registerSkyWarsDaboross();
         if (hookedPlugins.isEmpty()) {
             getLogger().warning("Did not find any plugins to hook into.");
         } else {
